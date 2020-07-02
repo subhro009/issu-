ff8d9347cb88b16bc5b9bb56e575924df6089f40
subhro009 <67740093+subhro009@users.noreply.github.com>
2020-07-02 16:50:00 +0530

    Add files via upload

---
+++ issu.sh
@@ -0,0 +1,60 @@
+echo " "
+echo  $p 'Please Wait ===+['$b'./                   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'....\                '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'...../               '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'......./             '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........\            '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........../          '$p']\'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']|'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'..............\      '$p']/'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']\'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'................/    '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|'
+sleep 0.4
+clear
+
+echo -e "\e[92m██╗███████╗██╗  ██╗██╗██╗  ██╗ █████╗ 
+██║██╔════╝██║  ██║██║██║ ██╔╝██╔══██╗
+██║███████╗███████║██║█████╔╝ ███████║
+██║╚════██║██╔══██║██║██╔═██╗ ██╔══██║
+██║███████║██║  ██║██║██║  ██╗██║  ██║
+╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                      \e[0m"
+echo -e "\e[31mCODED BY MrViruaS\e[0m"
+
+echo -e "\e[33mIssu a cute girl\e[0m"
